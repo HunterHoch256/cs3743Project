74d768d76db437f453c1abc7d6c6c1879fee8de7
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-13 23:25:20 -0600

    Facility data was handpicked; started on rand NAD script

---
+++ scripts/nad.py
@@ -0,0 +1,26 @@
+# nad.py
+# An application that uses the heap queue algorithm (described here https://docs.python.org/3/library/heapq.html) to select a given random number of records from the given filename
+# (implemented here: https://stackoverflow.com/questions/22130548/extracting-a-random-line-in-a-file-without-loading-the-file-into-ram-in-python).
+import heapq
+import random
+import sys
+
+if( len(sys.argv) < 4 ):
+    print( "nad.py syntax: ./nad.py <path to input file> <number of elements to select> <path to output file>" )
+
+# 1. Input file path
+input_file = sys.argv[1]
+
+# 2. Number of elements
+num_lines = int( sys.argv[2] )
+
+# 3. Output file path
+output_file = sys.argv[3]
+
+# Read input file
+with open( input_file, 'r' ) as input_file_obj:
+    # the "key" here is supposed to define a function with one parameter, which is used to extract a comparison key for each element.
+    # since the key just outputs a random number, the line selection will be random.
+    rand_lines = heapq.nlargest( num_lines, input_file_obj, key=(lambda L: random.random()))
+
+print(rand_lines)
